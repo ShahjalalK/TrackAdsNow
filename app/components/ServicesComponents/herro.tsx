51e67d65835bfc18f712d36934a0e8abc6211db8
shahjalalk <shahjalalkhan215@gmail.com>
2025-01-18 11:32:43 +0600

    again change herro bannar

--- app/components/ServicesComponents/herro.tsx
+++ app/components/ServicesComponents/herro.tsx
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import EmailSignatureMockup from "@/public/outlokgif.gif"
+import EmailSignatureMockup from "@/public/outlookgif3.gif"
 import EmailSignatureMockupe from "@/public/gmailGif.gif"
 import GmailMockup1 from "@/public/gmail1.png"
 import GmailMockup2 from "@/public/gmail2.png"
 import GmailMockup3 from "@/public/gmail3.png"
 import GmailMockup4 from "@/public/gmail4.png"
 import FiverrLinkButton from '../contact/fiverrLinkButton'
+
+import OutlookIcon from "@/public/outlookIcon.png"
+import AppleMail from "@/public/appleIcon.png"
+import GmailIcon from "@/public/gmailIcon.png"
+import YahooIcon from "@/public/yahooIcon.png"
 
 
 type Props = {}
@@ -18,10 +23,23 @@ const Herro = (props: Props) => {
 
        
 
-        <div className="container grid grid-cols-1 xl:grid-cols-5 gap-y-10 xl:gap-y-0 xl:gap-x-10">
+        <div className="container grid grid-cols-1 xl:grid-cols-7 gap-y-10 xl:gap-y-0 xl:gap-x-10">
             
-        <div className=" flex flex-col space-y-10 xl:col-span-4 ">
+        <div className=" flex flex-col space-y-10 xl:col-span-6 ">
                 <Image src={EmailSignatureMockup} alt="skupark" className="w-full" loading="lazy" />
+                <div className="flex xl:hidden flex-col items-center justify-center  space-y-5">
+             <div className="flex items-center space-x-4">
+             <h2 className="text-xl font-bold border rounded py-3 px-10 bg-white text-gray-800 shadow">Supported-</h2>
+             </div>
+
+             <div className="flex  items-center space-x-5">
+             <Image src={GmailIcon} alt="Gmail Icon"  className="w-10 inline-flex " />
+              <Image src={OutlookIcon} alt="Gmail Icon"  className="w-10 inline-flex " />
+              <Image src={AppleMail} alt="Gmail Icon"  className="w-10 inline-flex " />
+              <Image src={YahooIcon} alt="Gmail Icon"  className="w-10 inline-flex " />
+             </div>
+             
+            </div>
 
 
                 <div className="flex flex-col space-y-3 ">
@@ -40,6 +58,15 @@ const Herro = (props: Props) => {
 
             </div>
                           
+            </div>
+
+
+            <div className="hidden xl:flex flex-col space-y-5 items-center">
+              <h2 className="text-xl font-bold border border-t-0 rounded-b p-5 bg-white shadow">Supported</h2>
+              <Image src={GmailIcon} alt="Gmail Icon" className="w-16 grayscale hover:grayscale-0" />
+              <Image src={OutlookIcon} alt="Gmail Icon" className="w-16 grayscale hover:grayscale-0" />
+              <Image src={AppleMail} alt="Gmail Icon" className="w-16 grayscale hover:grayscale-0" />
+              <Image src={YahooIcon} alt="Gmail Icon" className="w-16 grayscale hover:grayscale-0" />
             </div>
 
             
